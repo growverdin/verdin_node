3bf8af6360c4647f490eedf55a9ac0d0bff0f190
André Roeck <andreroeck@Andres-MacBook-Air.local>
2016-09-15 00:18:20 -0300

    Backup made from local Arduino folder

---
+++ node.ino
@@ -0,0 +1,138 @@
+//Libraries
+#include <DHT.h>;
+
+void setup() {
+  Serial.begin(9600);
+  //dht.begin();
+
+  //tests
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+}
+
+void loop() {  
+  bool endOfMessage = false;
+  String message[50];
+  int commandCount = 0;
+  String serialRead;
+
+  int debug = 0;
+
+  //RECEIVE MESSAGE
+  while(!endOfMessage) {
+    if ( Serial.available() > 0 ) {
+      serialRead = Serial.readString();
+
+      if (serialRead.equalsIgnoreCase("startingMessage")) {
+        commandCount = 0;
+        Serial.write("sendNext");
+      } else {
+        while (serialRead.indexOf("/") > -1) {
+          //Serial.print("TEM " + serialRead);
+          message[commandCount] += serialRead.substring(0, serialRead.indexOf("/"));
+          serialRead = serialRead.substring(serialRead.indexOf("/")+1, serialRead.length());
+          commandCount++;
+        }
+
+        if (serialRead.equalsIgnoreCase("*")) {
+          endOfMessage = true;
+        } else {
+          message[commandCount] += serialRead.substring(0, serialRead.length());
+          Serial.write("sendNext");
+        }
+      }
+    }
+  }
+  
+  //EXECUTE
+  //ID-Pin-Pin/
+  //ID-Pin[value]-Pin[value]/
+
+  delay(500);
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  
+  if (commandCount > 0) {
+    String response[15];
+    int responseCount = 0;
+
+    for (int i=0 ; i<commandCount ; i++) {
+      String command = message[i];
+      int action = command.substring(0, command.indexOf("-")).toInt();
+      command = command.substring(command.indexOf("-"), command.length());
+
+      //MEASUREMENTS
+      if (action == 1/*Moisture Sensor*/ || action == 3/*Temperature Sensor*/ || action == 4/*pH Sensor*/) {
+        //turn on power for sensors
+        pinMode(12, OUTPUT);
+        digitalWrite(12, HIGH);
+        delay(500);
+      }
+
+      //ACTUATIONS
+      if (action == 2) {
+        //maybe preparation for actuation
+      }
+      
+      int pin;
+      int value;
+      while (command.indexOf("-") > -1) {        
+        //get next pin
+        if (String(command.charAt(1)).equalsIgnoreCase("0")) {
+          pin = String(message[i].charAt(2)).toInt();
+        } else {
+          pin = message[i].substring(2, 4).toInt();
+        }
+
+        //check if it has value attached
+        if (String(command.charAt(3)).equalsIgnoreCase("[")) {
+          value = command.substring(command.indexOf('[')+1, command.indexOf("]")).toInt();
+        }
+
+        //perform action
+        //MEASUREMENTS
+        if (action == 1) {
+          pinMode(pin, INPUT);
+          response[responseCount] = String((int)(((analogRead(pin)-213)*-0.123456789)+98));
+          responseCount++;
+        } else if (action == 3) {
+          DHT dht(pin, DHT22);
+          dht.begin();
+          float temp = dht.readTemperature();
+          float hum = dht.readHumidity();
+          response[responseCount] = String(temp) + "/" + String(hum);
+          responseCount++;
+        } else if (action == 4) {
+          pinMode(pin, INPUT);
+          response[responseCount] = String(((float)analogRead(pin)*5.0/1024/6)*3.5);
+          responseCount++;
+        }
+        //ACTUATIONS
+        else if (action == 2) {
+  
+        }
+
+        //check if has more pins and erase the last one
+        if (command.indexOf("-", 1) > -1) {
+          command = command.substring(command.indexOf("-", 1), command.length());
+        } else {
+          command = "";
+        }
+      }
+      
+      //MEASUREMENTS
+      if (action == 1/*Moisture Sensor*/ || action == 3/*Temperature Sensor*/ || action == 4/*pH Sensor*/) {
+        //turn off power for sensors
+        //delay(500);
+        digitalWrite(12, LOW);
+      }
+    }
+    
+    for (int i=0 ; i<responseCount ; i++) {
+      Serial.print(response[i]);
+      delay(100);
+    }
+    Serial.print("endOfMessage");
+  }
+}
